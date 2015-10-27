027aa7a3b623f432914c71360222b0134a95b090
LADOSS IFPB <ladossifpb@gmail.com>
2015-10-27 15:57:25 -0300

    Hibernate inicializando junto com o contexto - José Renan

---
+++ IFOpenDoors_SERVICE/src/br/edu/ifpb/ifopendoors/hibernate/HibernateListener.java
@@ -0,0 +1,15 @@
+package br.edu.ifpb.ifopendoors.hibernate;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class HibernateListener implements ServletContextListener {  
+	  
+    public void contextInitialized(ServletContextEvent event) {  
+        HibernateUtil.getSessionFactory(); 
+    }  
+  
+    public void contextDestroyed(ServletContextEvent event) {  
+    	HibernateUtil.shutdown();;
+    }  
+} 
