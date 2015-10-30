1afdca3e18f06f74870d4c1dde50998fa82b98d3
erijonhson <erijonhson.os@gmail.com>
2015-10-30 10:24:34 -0300

    Versão atualizada do IFOpenDoorsApp

--- IFOpenDoorsApplication/app/src/main/java/ifpb/edu/br/util/Constantes.java
+++ IFOpenDoorsApplication/app/src/main/java/ifpb/edu/br/util/Constantes.java
@@ -2,7 +2,7 @@ package ifpb.edu.br.util;
 
 public class Constantes {
 
-	public static final String URL_WEB_SERVICE = "http://192.168.1.100:8080/IFOpenDoors_SERVICE";
+	public static final String URL_WEB_SERVICE = "http://150.165.80.253:8080/IFOpenDoors_SERVICE";
 	public static final String OPEN_DOOR = "/door/open";
 	public static final String CLOSE_DOOR = "/door/closeDoor";
 }
