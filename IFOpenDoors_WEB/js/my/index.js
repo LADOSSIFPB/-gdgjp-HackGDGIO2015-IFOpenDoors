bd427f3c184dd6b5439424fff773a964f56dc796
Gustavo <gvilardefarias@ymail.com>
2016-07-20 00:43:36 -0300

    Login

--- IFOpenDoors_WEB/js/my/index.js
+++ IFOpenDoors_WEB/js/my/index.js
@@ -1,20 +1,18 @@
 angular.module('ifopendoors', []);
-angular.module("ifopendoors").controller("ifopendoorsCtrl", ['$scope', '$http', function($scope, $http) {
-	$scope.error = false;
+angular.module("ifopendoors").controller("ifopendoorsCtrl", function ($scope, $http, $location, $window) {
+	$scope.UNAUTHORIZED = false;
 
-	$scope.loginValidation = function (login) {        
-        var config = {
-            headers : {
-                'Content-Type': 'application/json'
-            }
+    $scope.loginValidation = function (login) {
+        console.log(login);
+        $http.post("http://localhost:8080/IFOpenDoors_SERVICE/person/login", login).success(function (data) {
+          var url = "http://" + $window.location.host + "/gerenciarSalas.html";
+          window.location = "/gerenciarSalas.html";
+      }).error( function (data, status, headers, config) {
+        if(status==401){
+            $scope.UNAUTHORIZED = true;
         }
+    });
+  };
 
-        $http.post('http://127.0.0.1:8080/IFOpenDoors_SERVICE/person/login', login, config)
-        .success(function (data, status, headers, config) {
-            $location.path('adiconarSala.html');
-        })
-        .error(function (data, status, header, config) {
-            $scope.error = true;
-        });
-    };
-}]);
+  $().alert('close');
+});
