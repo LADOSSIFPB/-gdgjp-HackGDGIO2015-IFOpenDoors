3a343316f315fa14fd50f80a8b77f67f6c6ae9f2
Gustavo Vilar <gvilardefarias@ymail.com>
2016-08-15 22:41:53 -0300

    Update ESP code

--- IFOpenDoors_ESP8266/Cliente-Server.py
+++ IFOpenDoors_ESP8266/Cliente-Server.py
@@ -1,50 +1,46 @@
-import RestServer
-import network
-from machine import Pin
-import ujson
-import lwip
-
 p = Pin(4, Pin.OUT)
-ip = "192.168.2.110"
 portaServidorESP = 8080
 
 def open():
-	print('Abrindo')
-	p.low()
+	p.high()
+	print('Aberto')
 	response = Response()
 	response.code(200)
 	response.contentType("text/plain")
-	response.data("Aberto")
+	response.data("Ligado")
 	return response.build()
 def close():
-	print('Fechando')
-	p.high()
+	p.low()
+	print('Fechado')
 	response = Response()
 	response.code(200)
 	response.contentType("text/plain")
-	response.data("Fechado")
+	response.data("Desligado")
+	return response.build()
+def isOn():
+	response = Response()
+	response.code(200)
 	return response.build()
 
 paths = {"/open":open,
-	 	"/close":close}
+	 "/close":close,
+	 "/isOn":isOn}
 
+p.high()
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect('arduino', 'arduino%')
+
+if not wlan.isconnected():
+	wlan.connect('Vilar', 'defarias')
 
 while not wlan.isconnected():
 	pass
 
 data = ujson.dumps({'ip':wlan.ifconfig()[0] + ":" + str(portaServidorESP)})
 
-requisicao = "POST /IFOpenDoors_SERVICE/door/insert HTTP/1.0" + "\r\n" + "Host: " + ip + "\r\n" + "User-Agent: ESP" + "\r\n" + "Accept: application/json" + "\r\n" + "If-modified-since: Sat, 29 Oct 1999 19:43:31 GMT" + "\r\n" + "Content-Type: application/json" + "\r\n" + "Content-Length: " + str(len(data)) + "\r\n" + "Connection: keep-alive" + "\r\n" + "\r\n" + data
+POST(data, "/IFOpenDoors_SERVICE/door/insert", "192.168.0.8", 8080)
 
-addr = lwip.getaddrinfo(ip, 8080)
-s = lwip.socket()
-s.connect(addr[0][-1])
-s.send(requisicao.encode())
-data = s.recv(1000)
-s.close()
+p.low()
 
-server = RestServer.Server(portaServidorESP)
+server = Server(portaServidorESP)
 server.start(paths)
